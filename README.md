30fcd541b2ffaaad5a48be3595958f827eb98d09
Jess Kenney <seveneightn9ne@gmail.com>
2015-11-28 13:24:00 -0500

    ExpectedLiteral on verb

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 6.S083 Final Project
 ## Semantic Parser
+
+```
+$ sbt
+> run input/intrans.txt
+```
+
+Why is there an ExpectedLiteral error?
--- src/main/scala/EnglishParser.scala
+++ src/main/scala/EnglishParser.scala
@@ -10,11 +10,13 @@ object EnglishParser extends SentenceParser with RegexParsers {
 
   lazy val sentence = np ~ " " ~ vp <~ "." ^^ {(np,_, vp) => Sentence(np, vp)}
   lazy val np = noun  ^^ {noun => NP(noun)}
-  lazy val noun = """\w+""".r ^^ {w => Noun(w)}
-  lazy val vp = ( verb          ^^ {v => VP(v, None)}
-                | verb ~ " " ~ np ^^ {(v,_,np) => VP(v, Some(np))}
-  )
-  lazy val verb = """\w+""".r ^^ {w => Verb(w)}
+  lazy val noun = "\\w+".r ^^ {w => Noun(w)}
+  //lazy val vp = ( verb          ^^ {v => VP(v, None)}
+  //              | verb ~ " " ~ np ^^ {(v,_,np) => VP(v, Some(np))}
+  //)
+  //lazy val vp = verb ~ " " ~ np ^^ {(v,_,np) => VP(v, Some(np))}
+  lazy val vp = verb ^^ {v => VP(v, None)}
+  lazy val verb = "\\w+".r ^^ {w => Verb(w)}
 
   def parser = sentence
 
