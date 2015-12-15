ebd2512f1b1c4a0e3639441012a2e0f4f7cfd097
Jess Kenney <seveneightn9ne@gmail.com>
2015-12-15 14:59:12 -0500

    with add

---
+++ src/main/scala/Utils.scala
@@ -0,0 +1,51 @@
+package semanticparser
+import XPrules._
+
+object Utils {
+
+  /** Recursively apply f n times to initial */
+  def refeed[A](n:Int, f:A => A, initial:A):A = n match {
+    case 0 => initial
+    case _ => refeed(n-1, f, f(initial))
+  }
+
+  def expandBitstring(set:List[List[Boolean]]):List[List[Boolean]] = set flatMap { bitstring =>
+    Set[List[Boolean]](true :: bitstring, false :: bitstring)
+  }
+
+
+  /** Pretty print tree */
+  def prettyprint(xp:XP[Word,Word,Word,Word]):String = xp.getClass.getSimpleName +
+    (xp.spec match {
+      case Some(spec) => "\n├─ " + prettyprint(spec).split("\n").mkString("\n│  ") + "\n"
+      case None => "\n"
+    }) + (xp.head match {
+      case Left(xbar) => "└─ " +  prettyprint(xbar).split("\n").mkString("\n   ")
+      case Right(conj) => "   " + prettyprint(conj).split("\n").mkString("\n   ")
+    })
+
+  def prettyprint(xbar:Xbar[Word,Word,Word]):String = xbar.getClass.getSimpleName +
+    (xbar.complement match {
+      case Some(c) => "\n├─ "
+      case None => "\n└─ "
+    }) + (xbar.head match {
+      case Left(h) => h.getClass.getSimpleName + " \033[1m" + h.asText + "\033[0m\n"
+      case Right(h) => prettyprint(h).split("\n").mkString("\n   ")
+    }) + (xbar.complement match {
+      case Some(c) => "└─ " + prettyprint(c).split("\n").mkString("\n   ")
+      case None => ""
+    }) + (xbar.adjunct match {
+      case Some(a) =>  "   " + prettyprint(a).split("\n").mkString("\n│  ")
+      case None => ""
+    })
+
+  def prettyprint(conj:ConjP[XP[Word,Word,Word,Word]]):String =
+    "Conjunction" + "\n" + (conj.preconj match {
+      case Some(p) => "  Preconj " + p.asText + "\n"
+      case _ => ""
+    }) + "  " + prettyprint(conj.left).split("\n").mkString("\n  ") +
+    "  " + conj.conj.asText + "\n  " + prettyprint(conj.right).split("\n").mkString("\n ")
+
+
+
+}
